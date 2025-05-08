874e3b9bc114588f684311448768fc3f61e3f6de
Qodicat <1357016290@qq.com>
2025-05-08 13:36:49 +0800

    fix：实现不在tmux里面的连续训练

--- multi_pre_training.sh
+++ multi_pre_training.sh
@@ -4,7 +4,7 @@ source ~/anaconda3/bin/activate llama_factory
 
 for task_id in {0..5}
 do
-  echo "开始训练任务 task_${task_id}"
-  python ./multi_pre_training.py --config_path ./config/multi_pre_training.json --task_name task_"${task_id}"
-  echo "任务 task_${task_id} 训练完成"
+  echo "开始训练任务 task_${$task_id}"
+  python ./multi_pre_training.py --config_path ./config/multi_pre_training.json --task_name task_"${$task_id}"
+  echo "任务 task_${$task_id} 训练完成"
 done
